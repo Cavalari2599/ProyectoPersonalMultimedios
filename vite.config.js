368ae75b83fd3b89350c4a72aa39a1f54f2b7e0b
Cavalari <186237549+Cavalari2599@users.noreply.github.com>
2026-06-04 23:01:30 -0600

    configura base (Vite, fuentes, favicon) y limpia scaffold inicial

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      // ion-icon es un web component (ionicons), no un componente Vue.
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag === 'ion-icon',
+        },
+      },
+    }),
+  ],
 })
